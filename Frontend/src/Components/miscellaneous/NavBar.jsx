9b6a368b4b7876c974cc2aa68860b0768727c4ba
Robinbisht394 <robinbisht394@gmail.com>
2025-12-15 19:51:20 +0530

    fix navigation buttons

--- Frontend/src/Components/miscellaneous/NavBar.jsx
+++ Frontend/src/Components/miscellaneous/NavBar.jsx
@@ -1,41 +1,40 @@
 import React from "react";
 import { FaUserCircle } from "react-icons/fa";
-
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="bg-white shadow-sm py-4 px-4 sm:px-6 lg:px-8 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Logo or App Name - Left Side */}
-        <a href="/" className="text-2xl font-bold text-blue-600 tracking-tight">
-          MeetNest
-        </a>
+        <h1 className="text-4xl font-extrabold text-blue-600">MeetNest</h1>
 
         <div className="flex items-center space-x-2 sm:space-x-4">
-          <a
-            href="/"
+          <NavLink
+            to={`/`}
             className="hidden sm:block text-gray-700 hover:text-blue-600 font-medium transition duration-150 py-2 px-3"
           >
             Home
-          </a>
-          <a
-            href="/dashboard"
-            className="hidden sm:flex items-center text-gray-700 hover:text-blue-600 transition duration-150 py-2 px-3"
+          </NavLink>
+          <NavLink
+            to={`/dashboard`}
+            className="hidden sm:block text-gray-700 hover:text-blue-600 font-medium transition duration-150 py-2 px-3 md:flex md:justify-center md:items-center"
           >
-            <FaUserCircle className="mr-1" size={18} /> Dashboard
-          </a>
+            <FaUserCircle className="mr-1" size={18} /> <p>Dashboard</p>
+          </NavLink>
 
-          <a
-            href="/sign-up"
-            className="bg-blue-600 text-white px-4 py-2 rounded-lg font-medium text-sm sm:text-base hover:bg-blue-700"
+          <NavLink
+            to={`/sign-up`}
+            className="hidden sm:block text-gray-700 hover:text-blue-600 font-medium transition duration-150 py-2 px-3"
           >
             Sign Up
-          </a>
-          <a
-            href="/login"
-            className="border border-blue-600 text-blue-600 px-4 py-2 rounded-lg font-medium text-sm sm:text-base hover:bg-blue-50 transition duration-200"
+          </NavLink>
+
+          <NavLink
+            to={`/login`}
+            className="hidden sm:block text-gray-700 hover:text-blue-600 font-medium transition duration-150 py-2 px-3"
           >
-            Log In
-          </a>
+            Login
+          </NavLink>
         </div>
       </div>
     </nav>
